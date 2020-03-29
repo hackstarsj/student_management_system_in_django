50498ce114a0b6cc91f379151f03bb85918faf86
sanjeev kumar <sksanjeev362@gmail.com>
2020-03-29 13:36:01 +0530

    Added Edit Page for Student,Staff,Subject,Course

--- student_management_app/HodViews.py
+++ student_management_app/HodViews.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
@@ -6,7 +7,8 @@ from student_management_app.models import CustomUser, Staffs, Courses, Subjects,
 
 
 def admin_home(request):
-    return render(request,"hod_template/home_content.html")
+    user=request.user
+    return render(request,"hod_template/home_content.html",{"user":user})
 
 def add_staff(request):
     return render(request,"hod_template/add_staff_template.html")
@@ -66,6 +68,12 @@ def add_student_save(request):
         session_end=request.POST.get("session_end")
         course_id=request.POST.get("course")
         sex=request.POST.get("sex")
+
+        profile_pic=request.FILES['profile_pic']
+        fs=FileSystemStorage()
+        filename=fs.save(profile_pic.name,profile_pic)
+        profile_pic_url=fs.url(filename)
+
         try:
             user=CustomUser.objects.create_user(username=username,password=password,email=email,last_name=last_name,first_name=first_name,user_type=3)
             user.students.address=address
@@ -74,7 +82,7 @@ def add_student_save(request):
             user.students.session_start_year=session_start
             user.students.session_end_year=session_end
             user.students.gender=sex
-            user.students.profile_pic=""
+            user.students.profile_pic=profile_pic_url
             user.save()
             messages.success(request,"Successfully Added Student")
             return HttpResponseRedirect("/add_student")
@@ -123,3 +131,142 @@ def manage_course(request):
 def manage_subject(request):
     subjects=Subjects.objects.all()
     return render(request,"hod_template/manage_subject_template.html",{"subjects":subjects})
+
+def edit_staff(request,staff_id):
+    staff=Staffs.objects.get(admin=staff_id)
+    return render(request,"hod_template/edit_staff_template.html",{"staff":staff})
+
+def edit_staff_save(request):
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        staff_id=request.POST.get("staff_id")
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        email=request.POST.get("email")
+        username=request.POST.get("username")
+        address=request.POST.get("address")
+
+        try:
+            user=CustomUser.objects.get(id=staff_id)
+            user.first_name=first_name
+            user.last_name=last_name
+            user.email=email
+            user.username=username
+            user.save()
+
+            staff_model=Staffs.objects.get(admin=staff_id)
+            staff_model.address=address
+            staff_model.save()
+
+            messages.success(request,"Successfully Edited Staff")
+            return HttpResponseRedirect("/edit_staff/"+staff_id)
+        except:
+            messages.error(request,"Failed to Edit Staff")
+            return HttpResponseRedirect("/edit_staff/"+staff_id)
+
+def edit_student(request,student_id):
+    courses=Courses.objects.all()
+    student=Students.objects.get(admin=student_id)
+    return render(request,"hod_template/edit_student_template.html",{"student":student,"courses":courses})
+
+def edit_student_save(request):
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        student_id=request.POST.get("student_id")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        address = request.POST.get("address")
+        session_start = request.POST.get("session_start")
+        session_end = request.POST.get("session_end")
+        course_id = request.POST.get("course")
+        sex = request.POST.get("sex")
+
+        if request.FILES['profile_pic']:
+            profile_pic=request.FILES['profile_pic']
+            fs=FileSystemStorage()
+            filename=fs.save(profile_pic.name,profile_pic)
+            profile_pic_url=fs.url(filename)
+        else:
+            profile_pic_url=None
+
+
+        try:
+            user=CustomUser.objects.get(id=student_id)
+            user.first_name=first_name
+            user.last_name=last_name
+            user.username=username
+            user.email=email
+            user.save()
+
+            student=Students.objects.get(admin=student_id)
+            student.address=address
+            student.session_start_year=session_start
+            student.session_end_year=session_end
+            student.gender=sex
+            course=Courses.objects.get(id=course_id)
+            student.course_id=course
+            if profile_pic_url!=None:
+                student.profile_pic=profile_pic_url
+            student.save()
+            messages.success(request,"Successfully Edited Student")
+            return HttpResponseRedirect("/edit_student/"+student_id)
+        except:
+            messages.error(request,"Failed to Edit Student")
+            return HttpResponseRedirect("/edit_student/"+student_id)
+
+def edit_subject(request,subject_id):
+    subject=Subjects.objects.get(id=subject_id)
+    courses=Courses.objects.all()
+    staffs=CustomUser.objects.filter(user_type=2)
+    return render(request,"hod_template/edit_subject_template.html",{"subject":subject,"staffs":staffs,"courses":courses})
+
+def edit_subject_save(request):
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        subject_id=request.POST.get("subject_id")
+        subject_name=request.POST.get("subject_name")
+        staff_id=request.POST.get("staff")
+        course_id=request.POST.get("course")
+
+        try:
+            subject=Subjects.objects.get(id=subject_id)
+            subject.subject_name=subject_name
+            staff=CustomUser.objects.get(id=staff_id)
+            subject.staff_id=staff
+            course=Courses.objects.get(id=course_id)
+            subject.course_id=course
+            subject.save()
+
+            messages.success(request,"Successfully Edited Subject")
+            return HttpResponseRedirect("/edit_subject/"+subject_id)
+        except:
+            messages.error(request,"Failed to Edit Subject")
+            return HttpResponseRedirect("/edit_subject/"+subject_id)
+
+
+def edit_course(request,course_id):
+    course=Courses.objects.get(id=course_id)
+    return render(request,"hod_template/edit_course_template.html",{"course":course})
+
+def edit_course_save(request):
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        course_id=request.POST.get("course_id")
+        course_name=request.POST.get("course")
+
+        try:
+            course=Courses.objects.get(id=course_id)
+            course.course_name=course_name
+            course.save()
+            messages.success(request,"Successfully Edited Course")
+            return HttpResponseRedirect("/edit_course/"+course_id)
+        except:
+            messages.error(request,"Failed to Edit Course")
+            return HttpResponseRedirect("/edit_course/"+course_id)
+
--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -40,4 +40,13 @@ urlpatterns = [
     path('manage_student', HodViews.manage_student),
     path('manage_course', HodViews.manage_course),
     path('manage_subject', HodViews.manage_subject),
+    path('edit_staff/<str:staff_id>', HodViews.edit_staff),
+    path('edit_staff_save', HodViews.edit_staff_save),
+    path('edit_student/<str:student_id>', HodViews.edit_student),
+    path('edit_student_save', HodViews.edit_student_save),
+    path('edit_subject/<str:subject_id>', HodViews.edit_subject),
+    path('edit_subject_save', HodViews.edit_subject_save),
+    path('edit_course/<str:course_id>', HodViews.edit_course),
+    path('edit_course_save', HodViews.edit_course_save),
+
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
